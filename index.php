0bcf1141ee6fbf84963467d436b6b075b84aa0be
Draimkedo <nscr00@hotmail.com>
2017-05-02 14:10:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>hola gays</title>
+        <title>santiago es gay</title>
     </head>
     <body>
         
