147f7471629decf78147c7e74137d674591a1567
SENA <SENA@DESKTOP-Q3AODOA>
2017-05-03 17:25:52 -0500

    dayanasjh

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Santiago es gay</title>
+        <title>Santiago no es gay</title>
     </head>
     <body>
         
